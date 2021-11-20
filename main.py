95f2fc95155a7e355c2e26d102d240e9df170332
Shakir <Shakir@NoEmail>
2021-11-19 21:46:21 -0600

    Initial working script

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import subprocess
+#!/bin/python3
+import time
 import logging
+from argparse import ArgumentParser
+from datetime import datetime
+from subprocess import Popen, PIPE, STDOUT
 """
     Script to rsync everything in a directory
 """
@@ -9,22 +13,52 @@ def backup():
     try:
         # Run in a targeted user's cron
         # rsync will copy the content of "/home/$USER" but not the $USER directory itself
-        subprocess.call(["rsync", "-Ccavz", "--delete", "/home/$USER/", "/path/dest"])
+        rsync = f"rsync -Ccavz --delete {args.src} {args.dest}"
+        process = Popen(rsync, shell=True, stdout=PIPE, stderr=STDOUT)
+
+        with process.stdout:
+            for i in iter(process.stdout.readline, b''):
+                logging.info(i.decode("utf-8").strip())
+
     except OSError as e:
-        logging.error(f"<backup> - Failed to execute \n {e}")
+        logging.error(f"backup() - Failed to run rsync\n{e}")
 
 
 def compress():
-    pass
+    today = datetime.today().strftime('%b-%d-%Y')
+    try:
+        # Compress on place
+        tar_file = f"{args.dest}/{today}-backup.tar.gz"
+        tar = f"tar czf {tar_file} --absolute-names {args.dest}"
+        process = Popen(tar, shell=True, stdout=PIPE, stderr=STDOUT)
+
+        with process.stdout:
+            for i in iter(process.stdout.readline, b''):
+                logging.info(i.decode("utf-8").strip())
+
+    except OSError as e:
+        logging.error(f"compress() - Failed to compress backup\n{e}")
 
 
-def ship():
-    pass
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename="/var/log/dirsync.log", filemode="a")
+                        filename="/var/log/syncdir/syncdir.log", filemode="a")
 
+    parser = ArgumentParser(prog='PROG',
+                            description='''Script to backup and compress a directory using rsync.
+                            Designed to run in a cron job. 
+                            You need to create a log directory in /var/log/syncdir 
+                            and you need to give the executing user permission to write to this directory.''')
+                            
+    parser.add_argument('--dest', required=True, help='Path to backup destination directory - no trailing forward-slash')
+    parser.add_argument('--src', required=True, help='Path to backup source directory - no trailing forward-slash')
+
+    args = parser.parse_args()
+
+    backup()
+    logging.info("Backup completed.")
+    # Arbitrary sleep
+    time.sleep(8)
+    compress()
+    logging.info("Backup compressed.")
